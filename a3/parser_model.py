a26a641b3a8497878039906d52f1cd7dc1cd1d3c
jiacong1990 <lijiacong1990@gmail.com>
2020-06-14 22:23:02 -0700

    A3 dev (#2)
    * Implemented minibatch_parse
    
    * Modified ParseModel.__init__
    
    * Implemented embedding_lookup
    
    * Implemented forward()
    
    * Corrected assignment error in parser_model, implemented train_for_epoch
    
    * Implemented train()
    
    * Added zero_grad step to train_for_epoch
    
    * Optimized embedding lookup function
    
    Co-authored-by: Jiacong Li <jiacongli@jiacongs-mbp.lan>

--- a3/parser_model.py
+++ a3/parser_model.py
@@ -70,9 +70,17 @@ class ParserModel(nn.Module):
         ###     nn.Parameter: https://pytorch.org/docs/stable/nn.html#parameters
         ###     Initialization: https://pytorch.org/docs/stable/nn.init.html
         ###     Dropout: https://pytorch.org/docs/stable/nn.html#dropout-layers
+        self.embed_to_hidden_weight = nn.Parameter(torch.empty(self.n_features * self.embed_size, self.hidden_size))
+        self.embed_to_hidden_bias = nn.Parameter(torch.empty(1, self.hidden_size))
+        nn.init.xavier_uniform_(self.embed_to_hidden_weight)
+        nn.init.uniform_(self.embed_to_hidden_bias)
 
+        self.dropout = nn.Dropout(p=self.dropout_prob)
 
-
+        self.hidden_to_logits_weight = nn.Parameter(torch.empty(self.hidden_size, self.n_classes))
+        self.hidden_to_logits_bias = nn.Parameter(torch.empty(1, self.n_classes))
+        nn.init.xavier_uniform_(self.hidden_to_logits_weight)
+        nn.init.uniform_(self.hidden_to_logits_bias)
 
         ### END YOUR CODE
 
@@ -103,9 +111,9 @@ class ParserModel(nn.Module):
         ###     Index select: https://pytorch.org/docs/stable/torch.html#torch.index_select
         ###     Gather: https://pytorch.org/docs/stable/torch.html#torch.gather
         ###     View: https://pytorch.org/docs/stable/tensors.html#torch.Tensor.view
-
-
-
+        batch_size, n_features = w.shape[0], w.shape[1]
+        x = torch.index_select(self.embeddings, 0, w.view(batch_size * n_features))
+        x = x.view(batch_size, -1)
         ### END YOUR CODE
         return x
 
@@ -140,7 +148,11 @@ class ParserModel(nn.Module):
         ### Please see the following docs for support:
         ###     Matrix product: https://pytorch.org/docs/stable/torch.html#torch.matmul
         ###     ReLU: https://pytorch.org/docs/stable/nn.html?highlight=relu#torch.nn.functional.relu
-
+        x = self.embedding_lookup(w)
+        hidden_raw = torch.matmul(x, self.embed_to_hidden_weight) + self.embed_to_hidden_bias
+        h = F.relu(hidden_raw)
+        h = self.dropout(h)
+        logits = torch.matmul(h, self.hidden_to_logits_weight) + self.hidden_to_logits_bias
 
         ### END YOUR CODE
         return logits
